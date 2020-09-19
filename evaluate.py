898e3cd152400ea9571deae312789247e84be709
mohsinkhn <mohsinjackspiro@gmail.com>
2020-09-19 08:44:37 +0000

    added evaluation script closure of netune after experiment

---
+++ evaluate.py
@@ -0,0 +1,60 @@
+import argparse
+import json
+import os
+
+from catalyst import dl
+from catalyst.dl import utils
+from catalyst.contrib.dl.callbacks.neptune_logger import NeptuneLogger
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import StratifiedKFold
+import torch
+from torch import nn
+from torch.utils.data import DataLoader
+
+# from torchcontrib.optim import SWA
+
+from constants import FilePaths, TGT_COLS
+from datasets import RNAData
+from modellib.RNNmodels import RNAGRUModel
+from nn_trainer import get_predictions, validation_metrics
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_folder", required=True)
+    args = parser.parse_args()
+
+    model_path = Path(args.model_folder)
+    folds = model_path.glob("fold_*")
+    folds = [int(fold.stem.split("_")[1]) for fold in folds]
+    with open(str(model_path / "hparams.json"), "r") as fp:
+        hparams = json.load(fp)
+
+    NUM_WORKERS = 8
+    BATCH_SIZE = hparams.get("batch_size", 32)
+    FP = FilePaths("data")
+    train = pd.read_json(FP.train_json, lines=True)
+    cvlist = list(
+        StratifiedKFold(hparams.get("num_folds"), shuffle=True, random_state=hparams.get("seed")).split(
+            train, train["SN_filter"]
+        )
+    )
+
+    device = utils.get_device()
+    val_preds = np.zeros(shape=(len(train), hparams["max_seq_pred"], hparams["num_features"]), dtype='float64')
+    for fold in folds:
+        val_idx = cvlist[fold][1]
+        vl = train.iloc[val_idx]
+        vl_ds = RNAData(vl, targets=TGT_COLS)
+        vl_dl = DataLoader(vl_ds, shuffle=False, drop_last=False, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
+        model = RNAGRUModel(hparams)
+        model.load_state_dict(torch.load(model_path / f"fold_{fold}" / "checkpoints/best.pth")["model_state_dict"])
+        val_preds[val_idx] = get_predictions(model, vl_dl, device)[:, :, : hparams["num_features"]]
+
+    y_trues = np.dstack((np.vstack(train[col].values) for col in TGT_COLS))
+    sn_flag = train["SN_filter"].values.astype(bool)
+    eval_results = validation_metrics(y_trues, val_preds, sn_flag)
+    print(eval_results)
--- nn_trainer.py
+++ nn_trainer.py
@@ -22,7 +22,7 @@ from modellib.RNNmodels import RNAGRUModel
 
 
 def mcrmse(y_true, y_pred):
-    return [mean_squared_error(y_true[:, :, i], y_pred[:, :, i], squared=False) for i in range(y_true.shape[1])]
+    return [mean_squared_error(y_true[:, :, i], y_pred[:, :, i], squared=False) for i in range(y_pred.shape[2])]
 
 
 class MCRMSE(nn.Module):
@@ -44,8 +44,8 @@ def train_one_fold(tr, vl, hparams, logger, logdir, device):
     tr_ds = RNAData(tr, targets=TGT_COLS)
     vl_ds = RNAData(vl, targets=TGT_COLS)
 
-    tr_dl = DataLoader(tr_ds, shuffle=True, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
-    vl_dl = DataLoader(vl_ds, shuffle=True, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
+    tr_dl = DataLoader(tr_ds, shuffle=True, drop_last=True, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
+    vl_dl = DataLoader(vl_ds, shuffle=False, drop_last=False, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
 
     model = RNAGRUModel(hparams)
     optimizer = torch.optim.Adam(model.parameters(), lr=hparams.get("lr", 1e-3), weight_decay=hparams.get("wd", 0))
@@ -85,19 +85,18 @@ def get_predictions(model, loader, device):
 
 
 def validation_metrics(y_trues, y_preds, sn_flag):
-    results = []
-    eval_cols = TGT_COLS
+    results = {}
     high_snr_losses = mcrmse(y_trues[sn_flag], y_preds[sn_flag])
-    for i, loss in enumerate(high_snr_losses):
-        results[f"high_snr_{eval_cols[i]}"] = loss
+    for col, loss in zip(TGT_COLS, high_snr_losses):
+        results[f"high_snr_{col}"] = loss
 
     low_snr_losses = mcrmse(y_trues[~sn_flag], y_preds[~sn_flag])
-    for i, loss in enumerate(low_snr_losses):
-        results[f"low_snr_{eval_cols[i]}"] = loss
+    for col, loss in zip(TGT_COLS, low_snr_losses):
+        results[f"low_snr_{col}"] = loss
 
     losses = mcrmse(y_trues, y_preds)
-    for i, loss in enumerate(losses):
-        results[f"{eval_cols[i]}"] = loss
+    for col, loss in zip(TGT_COLS, losses):
+        results[f"{col}"] = loss
 
     results["total_mcrmse"] = np.mean(losses)
     results["high_snr_mcrmse"] = np.mean(high_snr_losses)
@@ -119,37 +118,48 @@ if __name__ == "__main__":
     exp_dir.mkdir(exist_ok=True, parents=True)
 
     NUM_WORKERS = 8
+    NUM_FOLDS = hparams["num_folds"]
+
     BATCH_SIZE = hparams.get("batch_size", 32)
     FP = FilePaths("data")
     train = pd.read_json(FP.train_json, lines=True)
     cvlist = list(
-        StratifiedKFold(hparams.get("num_folds", 10), shuffle=True, random_state=hparams.get("seed", 978654)).split(
+        StratifiedKFold(hparams.get("num_folds"), shuffle=True, random_state=hparams.get("seed")).split(
             train, train["SN_filter"]
         )
     )
 
-    neptune_logger = NeptuneLogger(
-        api_token=os.environ["NEPTUNE_API_TOKEN"],
-        project_name="tezdhar/Covid-RNA-degradation",
-        name="covid_rna_degradation",
-        params=hparams,
-        tags=tags,
-        upload_source_files=["*.py", "modellib/*.py"],
-    )
+    with open(str(exp_dir / "hparams.json"), "w") as hf:
+        json.dump(hparams, hf)
+
     device = utils.get_device()
-    val_preds = np.zeros(shape=(len(train), hparams["max_seq_pred"], hparams["num_features"]), dtype='float32')
+    val_preds = np.zeros(shape=(len(train), hparams["max_seq_pred"], hparams["num_features"]), dtype='float64')
     for fold_num, (tr_idx, val_idx) in enumerate(cvlist):
         tr, vl = train.iloc[tr_idx], train.iloc[val_idx]
         if hparams.get("filter_sn"):
             tr = tr.loc[tr["SN_filter"] == 1]
-
+            vl = vl.loc[vl["SN_filter"] == 1]
         logdir = exp_dir / f"fold_{fold_num}"
         logdir.mkdir(exist_ok=True)
+        neptune_logger = NeptuneLogger(
+            api_token=os.environ["NEPTUNE_API_TOKEN"],
+            project_name="tezdhar/Covid-RNA-degradation",
+            name="covid_rna_degradation",
+            params=hparams,
+            tags=tags+[f"fold_{fold_num}"],
+            upload_source_files=["*.py", "modellib/*.py"],
+        )
         trained_model, _, vl_dl = train_one_fold(tr, vl, hparams, neptune_logger, logdir, device)
-        val_preds[val_idx] = get_predictions(trained_model, vl_dl, device)[:, :, : hparams["num_features"]]
+        vds = RNAData(train.iloc[val_idx], targets=TGT_COLS)
+        vdl = DataLoader(vds, shuffle=False, drop_last=False, batch_size=BATCH_SIZE, num_workers=NUM_WORKERS)
+        val_preds[val_idx] = get_predictions(trained_model, vdl, device)[:, :, : hparams["num_features"]]
+
+        if fold_num + 1 != len(cvlist):
+            neptune_logger.experiment.stop()
 
     y_trues = np.dstack((np.vstack(train[col].values) for col in TGT_COLS))
-    sn_flag = train["SN_filter"].values
+    sn_flag = train["SN_filter"].values.astype(bool)
     eval_results = validation_metrics(y_trues, val_preds, sn_flag)
     for eval_name, eval_value in eval_results.items():
-        neptune_logger.experiment.add_metric(eval_name, eval_value)
+        neptune_logger.experiment.log_metric(eval_name, eval_value)
+    neptune_logger.experiment.stop()
